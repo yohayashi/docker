8b499a9dfe9499790422403d32f1c56fe7bf909b
Yoshihiro Hayashi <yohayashi@gmail.com>
2017-01-01 23:42:44 +0900

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t yohayashi/python ./python
+docker build -t yohayashi/cloud9 ./cloud9
+docker build -t yohayashi/jupyter ./jupyter
+docker build -t yohayashi/supervisor ./supervisor
---
+++ jupyter/conf/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.ip = '*'
+c.NotebookApp.password = u'sha1:fe518676dd69:29306b574ea9b2fba6f4fa3e47169f4a532dcfc4'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+
+# c.NotebookApp.certfile = u'/certs/server.crt'
+# c.NotebookApp.keyfile = u'/certs/server.key'
