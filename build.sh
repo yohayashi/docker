a21148295ebbbd82e72e965a3a12ab5453f8fcc4
Yoshihiro Hayashi <yohayashi@gmail.com>
2017-01-04 15:32:49 +0900

    create dhparam.pem for nginx in docker's build process.

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ docker build -t yohayashi/python ./python
 docker build -t yohayashi/cloud9 ./cloud9
 docker build -t yohayashi/jupyter ./jupyter
 docker build -t yohayashi/supervisor ./supervisor
+docker build -t yohayashi/nginx ./nginx
+
